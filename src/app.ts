48f04f6fb7616d9f8db7f780e695bf1e99a9fce2
ThiagoBarbosa05 <thiagon16@hotmail.com>
2023-12-27 14:59:03 -0300

    implement teste router

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,10 @@ app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
 
 app.use(cookieParser())
 
+app.get('/teste', (req, res) => {
+  res.send('hello world')
+})
+
 app.use(express.urlencoded({ extended: true }))
 
 app.use(usersRouter)
